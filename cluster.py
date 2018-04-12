5ade3476bf8eae98d54a1fb42ccfa619a29ca69e
ishaansang <ishaansang5@gmail.com>
2018-04-12 19:12:23 +0530

    cluster code added

---
+++ cluster.py
@@ -0,0 +1,73 @@
+import os
+from Queue import PriorityQueue
+from sets import Set
+
+def group(val,s1,s2,arr):
+	sum1 = 100000.0
+	sum2 = 100000.0
+
+	print val
+	for x in s1:
+		# print x
+		sum1 = min(arr[val][x],sum1)
+
+	for y in s2:
+		sum2 = min(arr[val][y],sum2)
+		# print y
+
+	print sum1,sum2
+	return sum1 > sum2
+		
+
+
+arr = []
+fp = open("dtw_data0.txt","r")
+for line in fp:
+	line = ' '.join(line.split())
+	line = line.strip()
+	line = line.replace(' ',',')
+	line = line.split(',')
+	line = map(float,line)
+	arr.append(line)
+
+
+q = PriorityQueue()
+
+for i in range(len(arr)):
+	for j in range(len(arr[0])):
+		q.put((-1*arr[i][j],i,j))
+
+s1 = set()
+s2 = set()
+s = set()
+item = q.get()
+
+s1.add(item[1])
+s2.add(item[2])
+
+s.add(item[1])
+s.add(item[2])
+
+while not q.empty():
+	item = q.get()
+	if item[1] in s and item[2] not in s:
+		temp = group(item[2],s1,s2,arr)
+		print temp
+		s.add(item[2])
+		if temp is False:
+			s1.add(item[2])
+		else :
+			s2.add(item[2])
+
+
+	if item[2] in s and item[1] not in s:
+		temp = group(item[1],s1,s2,arr)
+		print temp
+		s.add(item[1])
+		if temp is False:
+			s1.add(item[1])		
+		else :
+			s2.add(item[1])		
+
+	
+print 'here'
