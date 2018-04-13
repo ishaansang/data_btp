b091ba6c982a6ad86c577d200cc988e7adf0662f
ishaansang <ishaansang5@gmail.com>
2018-04-13 13:34:38 +0530

    minor changes

--- cluster.py
+++ cluster.py
@@ -1,6 +1,8 @@
 import os
 from Queue import PriorityQueue
 from sets import Set
+from sklearn.cluster import KMeans
+import numpy as np
 
 def group(val,s1,s2,arr):
 	sum1 = 100000.0
@@ -69,5 +71,10 @@ while not q.empty():
 		else :
 			s2.add(item[1])		
 
-	
+
+
+X = np.array(arr)
+kmeans = KMeans(n_clusters=2, random_state=None).fit(X)
+print kmeans.labels_	
+
 print 'here'
